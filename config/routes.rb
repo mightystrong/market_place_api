336b9a96bedf62161c01d303c93d345c4b01e037
Michael Price <mike@mightystrongmedia.com>
2015-08-26 13:28:18 -0600

    Added user model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MarketPlaceApi::Application.routes.draw do
+  devise_for :users
   # Api definition
   namespace :api, defaults: { format: :json },
                   constraints: { subdomain: 'api' }, path: '/' do
