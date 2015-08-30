e99333a299b7a1c1364431fb66854a117fbff7df
Michael Price <mike@mightystrongmedia.com>
2015-08-30 12:48:04 -0600

    Adds user serializer for customizing the json output

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,20 +1,14 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  # we concatenate the json format
-  before(:each) do
-    request.headers['Accept'] = "application/vnd.marketplace.v1, #{Mime::JSON}"
-    request.headers['Content-Type'] = Mime::JSON.to_s
-  end
-
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it "returns the information about a reporter on a hash" do
-      user_response = json_response
+      user_response = json_response[:user]
       expect(user_response[:email]).to eql @user.email
     end
 
@@ -26,11 +20,11 @@ describe Api::V1::UsersController do
     context "when is successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }
       end
 
       it "renders the json representation for the user record just created" do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eql @user_attributes[:email]
       end
 
@@ -39,18 +33,19 @@ describe Api::V1::UsersController do
 
     context "when is not created" do
       before(:each) do
-        #notice I'm not including the email
-        @invalid_user_attributes = { password: "12345678",
-                                     password_confirmation: "12345678" }
-        post :create, { user: @invalid_user_attributes }, format: :json
+        @invalid_user_attributes = {
+            password: "12345678",
+            password_confirmation: "12345678"
+          } #notice I'm not including the email
+        post :create, { user: @invalid_user_attributes }
       end
 
       it "renders an errors json" do
-        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json errors on why the user could not be created" do
+      it "renders the json errors on whye the user could not be created" do
         user_response = json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
@@ -62,29 +57,25 @@ describe Api::V1::UsersController do
   describe "PUT/PATCH #update" do
     before(:each) do
       @user = FactoryGirl.create :user
-      request.headers['Authorization'] =  @user.auth_token
+      api_authorization_header @user.auth_token
     end
 
     context "when is successfully updated" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,
-                         user: { email: "newmail@example.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "newmail@example.com" } }
       end
 
       it "renders the json representation for the updated user" do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eql "newmail@example.com"
       end
 
       it { should respond_with 200 }
     end
 
-    context "when is not created" do
+    context "when is not updated" do
       before(:each) do
-        @user = FactoryGirl.create :user
-        patch :update, { id: @user.id,
-                         user: { email: "bademail.com" } }, format: :json
+        patch :update, { id: @user.id, user: { email: "bademail.com" } }
       end
 
       it "renders an errors json" do
@@ -104,8 +95,8 @@ describe Api::V1::UsersController do
   describe "DELETE #destroy" do
     before(:each) do
       @user = FactoryGirl.create :user
-      api_authorization_header @user.auth_token #we added this line
-      delete :destroy, id: @user.auth_token
+      api_authorization_header @user.auth_token
+      delete :destroy, { id: @user.id }
     end
 
     it { should respond_with 204 }
